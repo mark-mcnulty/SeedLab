1fe59ea965075377affac0018cd6a6044811fde4
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-09-28 11:41:21 -0600

    fix

--- computer_vision/camera.py
+++ computer_vision/camera.py
@@ -27,7 +27,7 @@ class arducam:
 
     # make a setup function
     def setup(self):
-        self.camera.iso = 500
+        self.camera.iso = 200
         time.sleep(2)
 
         self.camera.shutter_speed = self.camera.exposure_speed
