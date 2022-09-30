bc7c61236bfcafe17d6f48313b6c6b74798cdab7
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-09-30 11:34:41 -0600

    fix

--- computer_vision/Mini_Project/camera.py
+++ computer_vision/Mini_Project/camera.py
@@ -40,7 +40,7 @@ class arducam:
         for i in range(N):
             self.capture()
             g = self.camera.awb_gains
-            print(g)
+            print(g(0), g(1))
 
         # self.camera.awb_mode = 'off'
         # self.camera.awb_gains = g
