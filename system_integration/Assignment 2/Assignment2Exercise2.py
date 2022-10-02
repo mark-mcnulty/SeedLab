69b6dcfaeb9068e8dc0560eb6ae0a7ce3f1818b2
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-10-02 14:54:33 -0600

    fixes

--- system_integration/Assignment 2/Assignment2Exercise2.py
+++ system_integration/Assignment 2/Assignment2Exercise2.py
@@ -7,14 +7,14 @@ bus = smbus.SMBus(1)
 # this is the slave address
 address = 0x04
 
-def writeNumber(value,offset):
+def writeNumber(value):
     #bus.write_byte(address, value)
-    bus.write_byte_data(address, offset, value)
+    bus.write_byte_data(address, value)
     return -1
 
 def readNumber():
     #number = bus.read_byte(address)
-    number = bus.read_byte_data(address, offset)
+    number = bus.read_byte_data(address)
     return number
 
 while True:
