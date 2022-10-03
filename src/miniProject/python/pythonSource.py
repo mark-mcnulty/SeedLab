353d9d7e0b95152db18f41b49b087088b92baf54
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-10-02 18:24:02 -0600

    added lcd screen code

--- src/miniProject/python/pythonSource.py
+++ src/miniProject/python/pythonSource.py
@@ -1,6 +1,9 @@
 import time
 import camera
 import i2c
+import board
+import digitalio
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
 # for RPI version 1, use “bus = smbus.SMBus(0)”
 bus = smbus.SMBus(1)
@@ -29,6 +32,19 @@ if __name__ == "__main__":
 
     # setup the bus connection 
     device = i2c.Device(address, 1)
+
+    # setup the lcd
+    lcd_rs = digitalio.DigitalInOut(board.D7)
+    lcd_en = digitalio.DigitalInOut(board.D8)
+    lcd_d7 = digitalio.DigitalInOut(board.D12)
+    lcd_d6 = digitalio.DigitalInOut(board.D11)
+    lcd_d5 = digitalio.DigitalInOut(board.D10)
+    lcd_d4 = digitalio.DigitalInOut(board.D9)
+    lcd_backlight = digitalio.DigitalInOut(board.D13)
+    lcd_columns = 16
+    lcd_rows = 2 
+    lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+    lcd.message = "hello\nWorld"
 
     # continuously capture images
     while True:
