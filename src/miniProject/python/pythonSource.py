4baa087e4199665c0363af38b5c56d5ee460e597
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-10-03 11:37:14 -0600

    lcd stuff

--- src/miniProject/python/pythonSource.py
+++ src/miniProject/python/pythonSource.py
@@ -23,6 +23,15 @@ address = 0x04
 
 
 if __name__ == "__main__":
+    lcd_columns = 16
+    lcd_rows = 2
+
+    i2c = board.I2C()
+
+    lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+    lcd.clear()
+    lcd.color = [100,0,0]
+
     # make the camera object
     cam = camera.arducam()
 
@@ -33,16 +42,6 @@ if __name__ == "__main__":
     device = i2c.Device(address, 1)
 
     # setup the lcd
-    lcd_rs = digitalio.DigitalInOut(board.D7)
-    lcd_en = digitalio.DigitalInOut(board.D8)
-    lcd_d7 = digitalio.DigitalInOut(board.D12)
-    lcd_d6 = digitalio.DigitalInOut(board.D11)
-    lcd_d5 = digitalio.DigitalInOut(board.D10)
-    lcd_d4 = digitalio.DigitalInOut(board.D9)
-    lcd_backlight = digitalio.DigitalInOut(board.D13)
-    lcd_columns = 16
-    lcd_rows = 2 
-    lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
     lcd.message = "hello\nWorld"
 
     # continuously capture images
