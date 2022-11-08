96cd74c189e4af1da79b85e9c6385c5b1f2167f6
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-11-08 13:26:43 -0700

    added fsm for python demo2

---
+++ computer_vision/Demo2/angleDistSend.py
@@ -0,0 +1,63 @@
+import camera
+
+# define gloabl variables
+ANGLE = 0
+DISTANCE = 0
+
+def look(cam):
+    # setup
+    ANGLE = 0
+    DISTANCE = 0
+
+    # take picture
+    cam.takePicture()
+
+    # check for object
+    # detect the markers
+    corners, ids, h, w = cam.detect_markers_undistorted()
+
+    # check if the markers are found
+    if ids is not None:
+        return calc
+    else:
+        return look
+
+
+
+def calc(cam):
+    # calculate the angle and distance of the aruco marker
+    corners, ids, h, w = cam.detect_markers_undistorted()
+
+    # calculate the angle
+    DISTANCE = cam.get_marker_distance_func("image.jpg", corners, ids, h, w)
+    ANGLE = cam.get_marker_angle("image.jpg", corners, ids, h, w)
+
+    return send
+
+def send(cam):
+    # 
+    print(DISTANCE)
+    print(ANGLE)
+
+    return look
+
+state_dictionary = {
+    look: "looking",
+    calc: "calc",
+    send: "sending"
+}
+
+
+# create main loop
+if __name__ == "__main__":
+    state = look
+
+    cam = camera.Camera()
+
+    try:
+        while True:
+            new_state = state(cam)
+            state = new_state
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+        print("Exiting")
