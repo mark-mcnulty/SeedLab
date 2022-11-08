673972886520d0d506ae06a010822661566d62c8
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-11-08 13:43:30 -0700

    idk

--- computer_vision/Demo2/angleDistSend.py
+++ computer_vision/Demo2/angleDistSend.py
@@ -1,16 +1,10 @@
 import camera
 
-# define gloabl variables
-ANGLE = 0
-DISTANCE = 0
-
-def look(cam):
-    # setup
-    ANGLE = 0
-    DISTANCE = 0
-
+def look(cam, angle, distance):
     # take picture
     cam.capture()
+    angle = 0
+    distance = 0
 
     # check for object
     # detect the markers
@@ -18,32 +12,23 @@ def look(cam):
 
     # check if the markers are found
     if ids is not None:
-        return calc
+        # calculate the angle
+        distance = cam.get_marker_distance_func("image.jpg", corners, ids, h, w)
+        angle = cam.get_marker_angle("image.jpg", corners, ids, h, w)
+
+        return send, angle, distance
     else:
-        return look
+        return look, angle, distance
 
 
-
-def calc(cam):
-    # calculate the angle and distance of the aruco marker
-    corners, ids, h, w = cam.detect_markers_undistorted()
-
-    # calculate the angle
-    DISTANCE = cam.get_marker_distance_func("image.jpg", corners, ids, h, w)
-    ANGLE = cam.get_marker_angle("image.jpg", corners, ids, h, w)
-
-    return send
-
-def send(cam):
+def send(cam, angle, distance):
     # 
-    print(DISTANCE)
-    print(ANGLE)
+    print(angle + " " + distance)
 
     return look
 
 state_dictionary = {
     look: "looking",
-    calc: "calc",
     send: "sending"
 }
 
@@ -53,11 +38,14 @@ if __name__ == "__main__":
     state = look
 
     cam = camera.arducam()
+    
+    angle = 0
+    distance = 0
 
     try:
         while True:
             print("main")
-            new_state = state(cam)
+            new_state, angle, distance = state(cam, angle, distance)
             state = new_state
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
