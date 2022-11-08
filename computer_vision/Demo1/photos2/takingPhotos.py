059601999a9c9cd725511737a9cf4a4e1444b5ec
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-11-07 19:19:15 -0700

    infinite

--- computer_vision/Demo1/photos2/takingPhotos.py
+++ computer_vision/Demo1/photos2/takingPhotos.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
             
             img = cv2.imread(name + '.jpg')
             cv2.imshow("image", img)
-            cv2.waitKey(500)
+            cv2.waitKey(0)
             cv2.destroyAllWindows()
 
     except: 
