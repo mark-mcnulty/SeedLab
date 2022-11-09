c5fc254f3f54e2de196065583417d2d2c7d32b87
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-11-08 19:40:51 -0700

    idk

--- src/Demo2/python/angleDistSend.py
+++ src/Demo2/python/angleDistSend.py
@@ -7,7 +7,8 @@ turnDriveDone = False
 address = 0x04
 
 def wait():
-    number = bus.read_byte_data(address)
+    # number = bus.read_byte_data(address)
+    number = bus.read_byte(address)
     time.sleep(2)
     print(number)
     if number == 1:
