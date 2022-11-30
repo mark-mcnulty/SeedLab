4e1c1c2d13b82abc82b6129fea55f67ed09c406a
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-11-30 11:35:27 -0700

    fixed

--- src/finalDemo/srcArduino/srcArduino.ino
+++ src/finalDemo/srcArduino/srcArduino.ino
@@ -120,10 +120,10 @@ void setup() {
 
   // setup communication
   // initialize i2c as slave
-//   Wire.begin(SLAVE_ADDRESS);
+  Wire.begin(SLAVE_ADDRESS);
 
 //   // define callbacks for i2c communication
-//   Wire.onReceive(receiveEvent);
+  Wire.onReceive(receiveEvent);
 //   // Wire.onRequest(requestEvent);
 }
 
@@ -270,32 +270,32 @@ void loop() {
 /* 
 DATA FROM PI
 */
-// void receiveEvent(int howMany) {
+void receiveEvent(int howMany) {
 
-//   for (int i = 0; i < howMany; i++) {
-//     temp[i] = Wire.read();
-//     temp[i + 1] = '\0'; //add null after ea. char
-//   }
+  for (int i = 0; i < howMany; i++) {
+    temp[i] = Wire.read();
+    temp[i + 1] = '\0'; //add null after ea. char
+  }
 
-//   //RPi first byte is cmd byte so shift everything to the left 1 pos so temp contains our string
-//   for (int i = 0; i < howMany; ++i) {
-//     temp[i] = temp[i + 1];
-//   }
-//   swag = temp ;
-//   index = swag.indexOf(' ') ;
-//   len = swag.length() ;
-//   distanceTemp = swag.substring(0,index) ;
-//   angleTemp = swag.substring(index, len - 1) ;
-//   markerDistanceTheta = (distanceTemp.toFloat() - driveCorrect) / r ;
-//   markerAngleRad = (angleTemp.toFloat() * PI) / 180 ;
-//   markerFound = true;
-//   Serial.print("distance: ");
-//   Serial.print(markerDistanceTheta);
-//   Serial.println();
-//   Serial.print("angle: ");
-//   Serial.print(markerAngleRad);
-//   Serial.println();
-// }
+  //RPi first byte is cmd byte so shift everything to the left 1 pos so temp contains our string
+  for (int i = 0; i < howMany; ++i) {
+    temp[i] = temp[i + 1];
+  }
+  swag = temp ;
+  index = swag.indexOf(' ') ;
+  len = swag.length() ;
+  distanceTemp = swag.substring(0,index) ;
+  angleTemp = swag.substring(index, len - 1) ;
+  markerDistanceTheta = (distanceTemp.toFloat() - driveCorrect) / r ;
+  markerAngleRad = (angleTemp.toFloat() * PI) / 180 ;
+  markerFound = true;
+  Serial.print("distance: ");
+  Serial.print(markerDistanceTheta);
+  Serial.println();
+  Serial.print("angle: ");
+  Serial.print(markerAngleRad);
+  Serial.println();
+}
 
 /* 
 SENDING TO PI
